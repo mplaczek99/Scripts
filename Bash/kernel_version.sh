025b68c1a6ba3621266c9dd7b3892013bb1133d6
mplaczek99 <mplaczek99@gmail.com>
2024-09-16 14:24:52 -0500

    Modified kernel_check.sh from LTS to Linux kernel

--- Bash/kernel_version.sh
+++ Bash/kernel_version.sh
@@ -4,12 +4,12 @@
 kernel_version=$(uname -r)
 
 # Define colors
-LTS_COLOR="#4682B4"   # Steel Blue for LTS
+LINUX_COLOR="#4682B4"   # Steel Blue for LTS
 ZEN_COLOR="#B22222"   # Firebrick Red for Zen
 
 # Check if it's LTS or Zen and output with color
 if [[ $kernel_version == *"lts"* ]]; then
-    echo -e "<span color=\"$LTS_COLOR\">LTS Kernel</span>"
+    echo -e "<span color=\"$LINUX_COLOR\">Linux Kernel</span>"
 elif [[ $kernel_version == *"zen"* ]]; then
     echo -e "<span color=\"$ZEN_COLOR\">Zen Kernel</span>"
 fi
