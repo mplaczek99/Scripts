b4bf0b2560f662d3bffba7f15cb45b42761061b6
mplaczek99 <mplaczek99@gmail.com>
2024-09-16 14:40:52 -0500

    Fixed a bug in kernel_check.sh and fixed a comment

--- Bash/kernel_version.sh
+++ Bash/kernel_version.sh
@@ -4,12 +4,12 @@
 kernel_version=$(uname -r)
 
 # Define colors
-LINUX_COLOR="#4682B4"   # Steel Blue for LTS
-ZEN_COLOR="#B22222"   # Firebrick Red for Zen
+LINUX_COLOR="#4682B4"   # Steel Blue for Linux
+ZEN_COLOR="#B22222"     # Firebrick Red for Zen
 
-# Check if it's LTS or Zen and output with color
-if [[ $kernel_version == *"lts"* ]]; then
-    echo -e "<span color=\"$LINUX_COLOR\">Linux Kernel</span>"
-elif [[ $kernel_version == *"zen"* ]]; then
+# Check if it's Zen or standard Linux kernel and output with color
+if [[ $kernel_version == *"zen"* ]]; then
     echo -e "<span color=\"$ZEN_COLOR\">Zen Kernel</span>"
+else
+    echo -e "<span color=\"$LINUX_COLOR\">Linux Kernel</span>"
 fi
